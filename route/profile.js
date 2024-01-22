3eeb8146d507259eed62bdf3f894f919ee262968
Shan <lxxshan33@gmail.comm>
2024-01-21 21:57:06 -0500

    correct syntax error

---
+++ route/profile.js
@@ -0,0 +1,78 @@
+//import modules
+import express from 'express'
+
+// import controllers
+import { getProfiles, updateProfile, deleteProfile } from '../controllers/profile.js'
+import User from '../model/user.js'
+import isLoggedIn from '../middleware/isLoggedIn.js'
+
+const router = express.Router()
+
+
+router.get('/profiles', isLoggedIn, async (req, res) => {
+    try{
+        const profiles = await getProfiles()
+        res.status(200).send({
+            allProfiles: `${profiles}`
+        })
+    }
+    catch(err){
+        res.status(500).send({
+            getProfilesError: `${err}`
+        })
+    }
+    
+})
+
+router.put('/profiles/:id', isLoggedIn, async (req, res) => {
+    try{
+        const id = req.params.id
+        console.log(req.user.user.profileId)
+        const loggedInUser = req.user.user
+        if (id === loggedInUser.profileId){
+            const updatedProfile = await updateProfile(id, req.body)
+            res.status(200).send({
+                updateProfileInfo:`${updatedProfile}`
+            })
+        }else{
+            res.status(500).send({
+                updateProfileError:`You have no permit to update this profile or profile not found`
+            })
+        }
+    }
+    catch(err){
+        res.status(500).send({
+            updateProfileError: `${err}`
+        })
+    }
+    
+})
+
+// This is not necessary. The profile is created and deleted whenever user is created and deleted.
+router.delete('/profiles/:id', isLoggedIn, async (req, res) => {
+    try{
+        const id = req.params.id
+        const loggedInUser = req.user.user
+        console.log( loggedInUser)
+        if (id === loggedInUser.profileId){
+            loggedInUser.profileId = ''
+            const deletedProfile = await deleteProfile(id)
+            res.status(200).send({
+                deletedProfileInfo:`${deletedProfile}`,
+                userUpdated: loggedInUser
+            })
+            
+        }else{
+            res.status(500).send({
+                deletedProfileError:`You have no permit to update this profile or profile not found`
+            })
+        }
+    }
+    catch(err){
+        res.status(500).send({
+            deleteProfileError: `${err}`
+        })
+    }
+})
+
+export default router
