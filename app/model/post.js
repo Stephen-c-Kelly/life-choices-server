24ba927234d11247e5e289ac001413242bdc391d
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-01-27 10:54:22 -0500

    second commit

--- app/model/post.js
+++ app/model/post.js
@@ -14,6 +14,7 @@ const postSchema = mongoose.Schema({
     profileId:{type: mongoose.Schema.Types.ObjectId, ref:'Profile'},
     username:{type:String}
 },
+
 {timestamps: true})
 
 const Post = mongoose.model('Post', postSchema)
